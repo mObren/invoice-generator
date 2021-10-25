4a32bfda7d901e7c75c92983561bf3dddcd60f8e
Milos Obrenovic <mobrenovic040@gmail.com>
2021-10-25 14:44:06 +0200

    Mail body modifying.

--- resources/views/emails/send-invoice.blade.php
+++ resources/views/emails/send-invoice.blade.php
@@ -1,6 +1,6 @@
 Dear {{$invoice->client->company_name}},
 
-Thank you for longtime partnership and chosing our services and products once again, as well.
+Thank you for longtime partnership and for choosing our services and products once again, as well.
 We are sending you your invoice as attachment.
 For all additional informations, please, contact us.
 Kind regards!
